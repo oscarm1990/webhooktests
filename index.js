60d3a2ef469ea7e655f20881f53f9c883d3c3156
oscarm1990 <124219163+oscarm1990@users.noreply.github.com>
2024-05-17 12:36:59 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// normal json response
+//app.post('/webhook', (req, res) => {
+ //   console.log('Received webhook:', req.body);
+ //   res.sendStatus(200);
+//});
+
+// using queries
+app.get("/apiqueries", (req, res) => {
+    // get the URL parameters passed by
+    // query with req.query
+    const queries = req.query;
+    res.send(queries);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
